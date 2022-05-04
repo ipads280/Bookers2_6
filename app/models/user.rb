a28acfaa44e9d303adc8d8360f5749bbafefc4d9
EC2 Default User <ec2-user@ip-172-31-83-11.ec2.internal>
2022-05-04 07:04:56 +0000

    応用課題４完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  has_many :relationships,                                        foreign_key: :following_id
+  has_many :followings, through: :relationships, source: :follower
+  
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :reverse_of_relationships, source: :following
 
   has_many :books
   has_many :book_comments, dependent: :destroy
@@ -15,5 +21,9 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def is_followed_by?(user)
+    reverse_of_relationships.find_by(following_id: user.id).present?
   end
 end
